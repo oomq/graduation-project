cd83f6f62545f190202177777ae7210728e0f9db
1543485515 <1543485515@qq.com>
2019-01-15 11:23:36 +0800

    1

--- data management/data management.cpp
+++ data management/data management.cpp
@@ -1,21 +1,107 @@
-﻿// data management.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include "pch.h"
+﻿#include "opencv2/objdetect.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <string>
 #include <iostream>
 
-int main()
+using namespace std;
+using namespace cv;
+
+static void drawQRCodeContour(Mat &color_image, vector<Point> transform);
+static void drawFPS(Mat &color_image, double fps);
+static int  liveQRCodeDetect(const string& out_file);
+
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n"; 
+	int return_code = 0;
+	string out_file_name;
+	for (;;) {
+		return_code = liveQRCodeDetect(out_file_name);
+
+	}
+
+	return return_code;
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+void drawQRCodeContour(Mat &color_image, vector<Point> transform)//画二维码轮廓
+{
+	if (!transform.empty())
+	{
+		double show_radius = (color_image.rows > color_image.cols)
+			? (2.813 * color_image.rows) / color_image.cols
+			: (2.813 * color_image.cols) / color_image.rows;
+		double contour_radius = show_radius * 0.4;
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+		vector< vector<Point> > contours;
+		contours.push_back(transform);
+		drawContours(color_image, contours, 0, Scalar(211, 0, 148), cvRound(contour_radius));
+
+		RNG rng(1000);
+		for (size_t i = 0; i < 4; i++)
+		{
+			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+			circle(color_image, transform[i], cvRound(show_radius), color, -1);
+		}
+	}
+}
+
+void drawFPS(Mat &color_image, double fps)  //显示FPS
+{
+	ostringstream convert;
+	convert << cvRound(fps) << " FPS (QR detection)";
+	putText(color_image, convert.str(), Point(25, 25), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 255), 2);
+}
+
+int liveQRCodeDetect(const string& out_file)//**检测程序**//
+{
+	VideoCapture cap(0);//开摄像头
+	if (!cap.isOpened())
+	{
+		cout << "Cannot open a camera" << endl;
+		return -4;
+	}
+
+	QRCodeDetector qrcode;
+	TickMeter total;
+	for (;;)
+	{
+		Mat frame, src, straight_barcode;
+		string decode_info;
+		vector<Point> transform;
+		cap >> frame;
+		if (frame.empty())
+		{
+			cout << "End of video stream" << endl;
+			break;
+		}
+		cvtColor(frame, src, COLOR_BGR2GRAY);   //把frame 转为灰度图src
+
+		total.start();
+		bool result_detection = qrcode.detect(src, transform);//检测器
+		if (result_detection)
+		{
+			decode_info = qrcode.decode(src, transform, straight_barcode);//解码器
+			if (!decode_info.empty()) {
+				cout << decode_info << endl;     //解码后输出一次返回
+				return  0;
+			}
+
+		}
+		total.stop();
+		double fps = 1 / total.getTimeSec();
+		total.reset();
+
+		if (result_detection) { drawQRCodeContour(frame, transform); }
+		drawFPS(frame, fps);
+
+		imshow("Live QR code detector", frame);//显示摄像头
+		char c = (char)waitKey(30);
+		if (c == 27)
+			break;
+		if (c == ' ' && !out_file.empty())
+			imwrite(out_file, frame); // TODO write original frame too
+	}
+	return 0;
+}
